2a2ac4bfae8e7691552b51963b1f4a635e9772a0
Luis Leal <luisrrleal@gmail.com>
2022-09-04 21:05:35 -0500

    Round to one decimal

--- p291.java
+++ p291.java
@@ -4,37 +4,56 @@ public class p291 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        do {
-            Double numReal_1 = sc.nextDouble();
-            String numImaginario_1 = sc.next();
+        Double numReal_1 = sc.nextDouble();
+        String numImaginario_1 = sc.next();
 
-            numeroComplejo v_numComplejo1 = new numeroComplejo(numReal_1, numImaginario_1);
+        numeroComplejo v_numComplejo1 = new numeroComplejo(numReal_1, numImaginario_1);
 
-            Double numReal_2 = sc.nextDouble();
-            String numImaginario_2 = sc.next();
+        Double numReal_2 = sc.nextDouble();
+        String numImaginario_2 = sc.next();
 
-            numeroComplejo v_numComplejo2 = new numeroComplejo(numReal_2, numImaginario_2);
+        numeroComplejo v_numComplejo2 = new numeroComplejo(numReal_2, numImaginario_2);
 
-            String operador = sc.next();
+        String operador = sc.next();
 
-            numeroComplejo result = v_numComplejo1.m_operation(v_numComplejo2, operador);
+        numeroComplejo result = v_numComplejo1.m_operation(v_numComplejo2, operador);
 
-            String operadorRespuesta;
+        String operadorRespuesta;
 
-            if (result.m_getIntPart() < 0) {
-                operadorRespuesta = "";
+        if (result.m_getIntPart() < 0) {
+            operadorRespuesta = "";
+
+        } else {
+            operadorRespuesta = "+";
+        }
+
+        if (result.a_numReal % 1 == 0) {
+            int v_numReal = (int) Math.round(result.a_numReal);
+            if (result.m_getIntPart() % 1 == 0) {
+                int v_numImaginario = 0;
+                v_numImaginario = (int) Math.round(result.m_getIntPart());
+                System.out.println(v_numReal + " " + operadorRespuesta + v_numImaginario + "i");
+                System.exit(0);
             } else {
-                operadorRespuesta = "+";
+                System.out.println(v_numReal + " " + operadorRespuesta + result.a_numImaginario);
+                System.exit(0);
             }
 
+        } else if (result.m_getIntPart() % 1 == 0) {
+            int v_numImaginario = (int) Math.round(result.m_getIntPart());
             if (result.a_numReal % 1 == 0) {
-                int v_numReal = (int) Math.round(result.a_numReal);
-                System.out.print(v_numReal + " " + operadorRespuesta + result.a_numImaginario);
+                int v_numReal = 0;
+                v_numReal = (int) Math.round(result.a_numReal);
+                System.out.println(v_numReal + " " + operadorRespuesta + v_numImaginario + "i");
+                System.exit(0);
             } else {
-                System.out.print(result.a_numReal + " " + operadorRespuesta + result.a_numImaginario);
+                System.out.println(result.a_numReal + " " + operadorRespuesta + v_numImaginario + "i");
+                System.exit(0);
             }
-
-        } while (true);
+        }
+        else{
+            System.out.println(result.a_numReal + " " + operadorRespuesta + result.a_numImaginario); 
+        }
     }
 }
 
@@ -56,21 +75,23 @@ class numeroComplejo {
         switch (operador) {
             case "+":
                 v_resultado.a_numReal = a_numReal + p_numeroComplejo.a_numReal;
-                v_resultado.a_numImaginario = Double.toString(a_intPart + p_intPart) + "i";
+                v_resultado.a_numImaginario = Double.toString(Math.round((a_intPart + p_intPart) * 10) / 10d) + "i";
                 break;
             case "-":
                 v_resultado.a_numReal = a_numReal - p_numeroComplejo.a_numReal;
-                v_resultado.a_numImaginario = Double.toString(a_intPart - p_intPart) + "i";
+                v_resultado.a_numImaginario = Double.toString(Math.round((a_intPart - p_intPart) * 10) / 10d) + "i";
                 break;
             case "*":
-                v_resultado.a_numReal = a_numReal * p_numeroComplejo.a_numReal;
-                v_resultado.a_numImaginario = Double.toString(a_intPart * p_intPart) + "i";
+                v_resultado.a_numReal = (double) Math.round(a_numReal * p_numeroComplejo.a_numReal);
+                v_resultado.a_numImaginario = Double.toString(Math.round((a_intPart * p_intPart) * 10) / 10d) + "i";
                 break;
             case "/":
                 v_resultado.a_numReal = a_numReal / p_numeroComplejo.a_numReal;
-                v_resultado.a_numImaginario = Double.toString(a_intPart / p_intPart) + "i";
+                v_resultado.a_numImaginario = Double.toString(Math.round((a_intPart / p_intPart) * 10) / 10d) + "i";
+
                 break;
         }
+        v_resultado.a_numReal = Math.round((v_resultado.a_numReal) * 10) / 10d;
         return v_resultado;
     }
 
